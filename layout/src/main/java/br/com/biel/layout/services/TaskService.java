cfc312ca6a91d28454b56ccd4cd6bbae9bf50444
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-01-17 13:33:35 -0300

    Metodos listarTarefas, delete, acharTarefa

--- layout/src/main/java/br/com/biel/layout/services/TaskService.java
+++ layout/src/main/java/br/com/biel/layout/services/TaskService.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import br.com.biel.layout.model.Task;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class TaskService {
 
@@ -15,6 +18,18 @@ public class TaskService {
         taskRepository.save(task);
     }
 
+    public List<Task> listarTarefas() {
+        return taskRepository.findAll();
+    }
 
+    public void delete(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    public Optional<Task> acharTarefa(Long id) {
+        Optional<Task> task = taskRepository.findById(id);
+
+        return task;
+    }
 
 }
