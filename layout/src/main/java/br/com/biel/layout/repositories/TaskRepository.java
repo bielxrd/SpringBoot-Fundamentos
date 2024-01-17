5431b2fabe0ca59bbf8c22e9d8f686d3f8f15dda
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-01-17 13:35:08 -0300

    Metodo findAllById

--- layout/src/main/java/br/com/biel/layout/repositories/TaskRepository.java
+++ layout/src/main/java/br/com/biel/layout/repositories/TaskRepository.java
@@ -3,5 +3,11 @@ package br.com.biel.layout.repositories;
 import br.com.biel.layout.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    public List<Task> findAllById();
+
 }
