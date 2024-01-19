d94a2a69da930c5b3ef9f42bd05297d411ecde70
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-01-19 15:16:48 -0300

    Fundamentos do Spring Boot (inicio)

---
+++ primeiro-projeto-springboot/src/main/java/br/com/gabrieldias/main/PrimeiroProjetoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrieldias.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoSpringbootApplication.class, args);
+	}
+
+}
