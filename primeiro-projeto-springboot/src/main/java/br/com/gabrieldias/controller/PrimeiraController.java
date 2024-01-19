8b8a18b26755b20b3662f6e157db3138ae335fb6
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-01-19 19:35:30 -0300

    Mudanca na controller utilizando ResponseBody e retornando um json do meu objeto + alteracoes de aonacao no metodo main

--- primeiro-projeto-springboot/src/main/java/br/com/gabrieldias/controller/PrimeiraController.java
+++ primeiro-projeto-springboot/src/main/java/br/com/gabrieldias/controller/PrimeiraController.java
@@ -1,18 +1,21 @@
-package br.com.gabrieldias.main.controller;
+package br.com.gabrieldias.controller;
 
 
+import br.com.gabrieldias.model.Objeto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // Essa anotacao diz que essa classe vai ser um controlador de uma aplicacao web
 @RequestMapping("/primeiraController") // Toda vez que eu acessar a url /primeiraController vai cair dentro desta classe
 public class PrimeiraController {
 
-    @GetMapping("/primeiroMetodo")
-    public String primeiroMetodo() {
-        return "Meu primeiro metodo API REST";
+    @GetMapping("/primeiroMetodo") // Toda vez que eu acessar a url /primeiraController/primeiroMetodo vai cair dentro deste metodo
+    @ResponseBody // Essa anotacao diz que o retorno deste metodo vai ser o corpo da resposta da requisicao
+    public Objeto primeiroMetodo() {
+        return new Objeto(1, "Gabriel");
     }
 
 
