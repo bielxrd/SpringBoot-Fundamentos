e79cecbe8282e9229981d2d0ca7240f24c43c151
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-01-19 20:16:19 -0300

    CrossOrigin para receber requisicoes de qualquer local, @PathVariable utilizada para parametros vindas da URL, QueryParams e @RequestParam para capturar valores de parametros de consulta de URL (apos o ?) e uma nova forma de lidar com diversas entradas de parametros pela url usando @RequestParam e Map

--- primeiro-projeto-springboot/src/main/java/br/com/gabrieldias/controller/PrimeiraController.java
+++ primeiro-projeto-springboot/src/main/java/br/com/gabrieldias/controller/PrimeiraController.java
@@ -3,18 +3,37 @@ package br.com.gabrieldias.controller;
 
 import br.com.gabrieldias.model.Objeto;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
 
 @RestController // Essa anotacao diz que essa classe vai ser um controlador de uma aplicacao web
+@CrossOrigin(origins = "*") // Essa anotacao diz que qualquer servidor pode fazer requisicoes para este controlador
 @RequestMapping("/primeiraController") // Toda vez que eu acessar a url /primeiraController vai cair dentro desta classe
 public class PrimeiraController {
 
-    @GetMapping("/primeiroMetodo") // Toda vez que eu acessar a url /primeiraController/primeiroMetodo vai cair dentro deste metodo
+
+    @GetMapping("/primeiroMetodo/{id}") // Toda vez que eu acessar a url /primeiraController/primeiroMetodo vai cair dentro deste metodo, o {id} e um parametro que eu posso passar na url
+    public String primeiroMetodo(@PathVariable Integer id) { // Essa anotacao diz que o parametro id vai ser o parametro que eu passei na url
+        return "O parametro é "+id;
+    }
+
+    // Query Params
+    @GetMapping("/metodoComQueryParams")
+    public String metodoComQueryParams(@RequestParam String id, @RequestParam(name = "nome") String name) { // RequestParam é uma anotacao para dizer que o parametro id vai ser um parametro que eu vou passar na url
+        return "O parametro com metodoQueryParams é "+id+ "e o parametro name é "+name;
+    }
+
+    @GetMapping("/metodoComQueryParams2")
+    public String metodoComQueryParams2(@RequestParam Map<String, String> allParams) { // @RequestParam com Map para pegar todos os parametros que eu passar na url
+        return "Os parametros com metodoQueryParams é "+allParams.entrySet();
+    }
+
+    @GetMapping("/metodoJson") // Toda vez que eu acessar a url /primeiraController/metodoJson vai cair dentro deste metodo
     @ResponseBody // Essa anotacao diz que o retorno deste metodo vai ser o corpo da resposta da requisicao
-    public Objeto primeiroMetodo() {
+    public Objeto metodoJson() {
+        System.out.println("Funcionou!");
         return new Objeto(1, "Gabriel");
     }
 
