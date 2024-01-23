d89160a531b26846a3bad2eb5542c6598b9c419d
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-01-23 16:05:40 -0300

    Mudanca na main para o spring boot gerenciar todas as classes

--- primeiro-projeto-springboot/src/main/java/br/com/gabrieldias/PrimeiroProjetoSpringbootApplication.java
+++ primeiro-projeto-springboot/src/main/java/br/com/gabrieldias/PrimeiroProjetoSpringbootApplication.java
@@ -1,4 +1,4 @@
-package br.com.gabrieldias.main;
+package br.com.gabrieldias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication // Utilizada para marcar qual a classe principal dentro de uma aplicação Spring Boot "Essa e a classe principal da minha aplicacao e tudo oq for acontecer depois de startar vai acontecer baseada nesta classe.
 // Spring Boot vai procurar uma classe comessa anotacao e sabe que o gerenciamento de toda a aplicacao vai ser baseado nessa classe.
-@ComponentScan(basePackages = "br.com.gabrieldias") // Essa anotacao diz que o Spring Boot vai procurar por todas as classes que estao dentro do pacote br.com.gabrieldias
+// @ComponentScan(basePackages = "br.com.gabrieldias") // Essa anotacao diz que o Spring Boot vai procurar por todas as classes que estao dentro do pacote br.com.gabrieldias
 public class PrimeiroProjetoSpringbootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeiroProjetoSpringbootApplication.class, args);
