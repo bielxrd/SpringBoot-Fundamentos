8b6511eec9995bb547fca88a6685117062f0c650
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-01-23 17:32:20 -0300

    Explicacao Ioc-DI

--- primeiro-projeto-springboot/src/main/java/br/com/gabrieldias/ioc_di/ControllerComponent.java
+++ primeiro-projeto-springboot/src/main/java/br/com/gabrieldias/ioc_di/ControllerComponent.java
@@ -13,6 +13,8 @@ public class ControllerComponent {
     @Autowired
     MeuComponent component;
 
+    // Ioc Di é um design pattern que diz que a classe não deve ser responsável por instanciar as suas dependências, mas sim receber as suas dependências por meio de injeção de dependência, ou seja, essa carga de trabalho fica para o framework, no caso o Spring Boot.
+
     @GetMapping("/")
     public String chamandoComponente() {
         String resultado = component.chamarMeuComponente();
