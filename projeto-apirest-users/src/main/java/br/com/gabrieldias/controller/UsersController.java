05ccf363fe708bd4a4fd235c5fac45d039cbe880
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-01-24 13:14:19 -0300

    Primeira rota feita para cadastro de usuario

---
+++ projeto-apirest-users/src/main/java/br/com/gabrieldias/controller/UsersController.java
@@ -0,0 +1,35 @@
+package br.com.gabrieldias.controller;
+
+
+import br.com.gabrieldias.entities.User;
+import br.com.gabrieldias.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/cadastro")
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+        List<User> users = userService.listagem(user);
+
+        if (users.size() > 0) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username ja existe");
+        } else {
+            userService.salvar(user);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Usuario criado com sucesso");
+        }
+
+
+    }
+
+
+}
