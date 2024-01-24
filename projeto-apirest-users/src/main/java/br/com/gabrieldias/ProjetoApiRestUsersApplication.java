1d59d0d2dbe9f320a603eec898a9991d0161629e
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-01-24 16:18:39 -0300

    Fix CrossOrigin

--- projeto-apirest-users/src/main/java/br/com/gabrieldias/ProjetoApiRestUsersApplication.java
+++ projeto-apirest-users/src/main/java/br/com/gabrieldias/ProjetoApiRestUsersApplication.java
@@ -2,8 +2,10 @@ package br.com.gabrieldias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*")
 public class ProjetoApiRestUsersApplication {
 
 	public static void main(String[] args) {
