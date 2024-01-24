6aab1991ba3fceb40c1b73883c9009cabbc36543
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-01-24 12:27:56 -0300

    Projeto API REST Cadastro e validacao de usuario

---
+++ projeto-apirest-users/src/test/java/br/com/gabrieldias/ProjetoApiRestUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrieldias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApiRestUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
