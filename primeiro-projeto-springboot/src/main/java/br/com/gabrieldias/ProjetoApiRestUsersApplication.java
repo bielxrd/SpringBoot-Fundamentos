932998cf4551190798341ab5f3dcc41e2cb1739c
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-01-24 13:14:00 -0300

    COnfiguracao do MySQL para Spring Boot

---
+++ primeiro-projeto-springboot/src/main/java/br/com/gabrieldias/ProjetoApiRestUsersApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrieldias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiRestUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiRestUsersApplication.class, args);
+	}
+
+}
